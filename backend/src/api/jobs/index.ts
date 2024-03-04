0bdf22c2ff0f40769682343a5005988d246ed8e9
Bitrey <alessandro.amella@live.it>
2024-03-04 01:32:55 +0100

    SCRIPT DEL CAZZO

--- backend/src/api/jobs/index.ts
+++ backend/src/api/jobs/index.ts
@@ -1,7 +1,5 @@
 import { logger } from "../../shared";
-import { cleanUnusedFilesJob } from "./cleanUnusedFiles";
 import { sendEqslEmailJob } from "./sendEmails";
 
 logger.debug("Starting Cron Jobs");
-cleanUnusedFilesJob.start();
 sendEqslEmailJob.start();
