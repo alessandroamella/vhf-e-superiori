b0b5264d3bba086ad2c7566a2a6c92d84069b8b6
Bitrey <alessandro.amella@live.it>
2024-03-09 19:12:45 +0100

    last fixes

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 import BurgerMenu from "./sideMenu/BurgerMenu";
 
 const Layout = ({ children }) => (
-  <div className="m-0 p-0" id="outer-container">
+  <div className="m-0 p-0 dark:bg-gray-700" id="outer-container">
     <BurgerMenu />
     <div id="page-wrap" className="m-0 p-0">
       <Header />
