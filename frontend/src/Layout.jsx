f62143652c688fee0553a7f01690025933994d27
Alessandro Amella <info@bitrey.it>
2025-03-01 18:12:34 +0100

    layout no children only outlet

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,24 +1,18 @@
-import PropTypes from "prop-types";
 import { Outlet } from "react-router";
 import Footer from "./Footer";
 import Header from "./Header";
 import BurgerMenu from "./sideMenu/BurgerMenu";
 
-const Layout = ({ children }) => (
+const Layout = () => (
   <div className="m-0 p-0 dark:bg-gray-700" id="outer-container">
     <BurgerMenu />
     <div id="page-wrap" className="m-0 p-0">
       <Header />
-      {children}
       <Outlet />
       <Footer />
     </div>
   </div>
 );
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired
-};
 
 Layout.displayName = "Layout";
 
